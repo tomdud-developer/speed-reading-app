5eb004ed6c0145b127683aececc45bfe8440f8fb
tomadud-developer <tomadud045@student.polsl.pl>
2022-09-24 20:15:57 +0200

    Initial commit - Spring Initializr

---
+++ src/main/java/com/src/speedreadingapp/SpeedReadingAppApplication.java
@@ -0,0 +1,13 @@
+package com.src.speedreadingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedReadingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedReadingAppApplication.class, args);
+	}
+
+}
