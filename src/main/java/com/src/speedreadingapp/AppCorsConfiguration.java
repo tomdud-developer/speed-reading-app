9e3c9abb381d649ad656a2f3ff15d0d0e99f5205
tomadud-developer <tomadud045@student.polsl.pl>
2022-09-27 16:59:01 +0200

    security config preset #2

---
+++ src/main/java/com/src/speedreadingapp/AppCorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.src.speedreadingapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class AppCorsConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+
+
+}
