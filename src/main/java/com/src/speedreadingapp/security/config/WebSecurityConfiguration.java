30713e026bef01d344cc824ec327786bd7b5b2ce
tomadud-developer <tomadud045@student.polsl.pl>
2022-09-27 16:48:06 +0200

    security config preset

---
+++ src/main/java/com/src/speedreadingapp/security/config/WebSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.src.speedreadingapp.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/api/login", "/api/v1/user/test2", "/api/v1/files/**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated();//and().formLogin();
+
+    }
+
+
+
+
+
+}
