d6602ce3a3c5a62337cab02e039ebc8e72c9cdd9
tomadud-developer <tomadud045@student.polsl.pl>
2022-10-03 22:24:31 +0200

    Registration System

--- src/main/java/com/src/speedreadingapp/registration/RegistrationController.java
+++ src/main/java/com/src/speedreadingapp/registration/RegistrationController.java
@@ -14,6 +14,7 @@ public class RegistrationController {
     }
 
     @PostMapping
+    @CrossOrigin
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
