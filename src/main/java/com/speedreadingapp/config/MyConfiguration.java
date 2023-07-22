d954a78f858ea96ab696287b1fa6246c6b0fa4b1
tomdud-developer <tomadud045@student.polsl.pl>
2023-07-22 14:11:00 +0200

    Add docker run -d -p 8081:8081 --name speedreadingappbackend speedreadingappbackend:latest --restart=always

---
+++ src/main/java/com/speedreadingapp/config/MyConfiguration.java
@@ -0,0 +1,30 @@
+package com.speedreadingapp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+
+@ConfigurationProperties("db")
+public class MyConfiguration {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
