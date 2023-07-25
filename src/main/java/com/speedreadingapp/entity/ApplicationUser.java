298dd17ddf12575a16173078d4cc8bd30536f7d9
tomdud-developer <tomadud045@student.polsl.pl>
2023-07-25 15:08:37 +0200

    Add ApplicationUser Entity and Role Enum

---
+++ src/main/java/com/speedreadingapp/entity/ApplicationUser.java
@@ -0,0 +1,53 @@
+package com.speedreadingapp.entity;
+
+import com.speedreadingapp.util.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Immutable;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "application_user")
+public class ApplicationUser {
+
+    @Id
+    @SequenceGenerator(
+            name = "application_user_sequence",
+            sequenceName = "application_user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "application_user_sequence"
+    )
+    @Immutable
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "enabled")
+    private Boolean enabled = false;
+
+    @Column(name = "account_creation_date")
+    private LocalDate accountCreationDate;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ src/main/java/com/speedreadingapp/util/Role.java
@@ -0,0 +1,5 @@
+package com.speedreadingapp.util;
+
+public enum Role {
+    ADMIN, MANAGER, USER, NOT_CONFIRMED
+}
